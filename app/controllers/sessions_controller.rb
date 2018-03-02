513cecf3ef139ced9b9615929f49e7a2176c1e36
pabloaredu <pablo.adu83@gmail.com>
2018-03-02 00:56:04 +0000

    Minor style changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,13 +13,13 @@ class SessionsController < ApplicationController
 			redirect_to '/'
 		else
 		# If user's login doesn't work, send them back to the login form.
-			redirect_to '/'
+			redirect_to '/sessions/new'
 		end
 	end
 
 	def destroy
     session[:user_id] = nil
-    redirect_to "/"
+    redirect_to "/sessions/new"
 	end
 
 end
