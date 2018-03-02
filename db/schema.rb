54c044bc6d2a5c746033432ad8b98d80ff9c3ccf
pabloaredu <pablo.adu83@gmail.com>
2018-03-02 00:28:31 +0000

    Added users and sessions

--- db/schema.rb
+++ db/schema.rb
@@ -57,9 +57,9 @@ ActiveRecord::Schema.define(version: 20180301200457) do
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "frist_name"
+    t.string   "first_name"
     t.string   "last_name"
-    t.string   "e_mail"
+    t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
